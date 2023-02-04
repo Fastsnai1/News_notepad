650412dc954912eb7ba8d62c36c9bee8b9decc22
Daniil <dankulagin@mail.ru>
2023-02-04 16:10:13 +0300

    added relationships between tables in the database

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class News(models.Model):
@@ -8,6 +9,20 @@ class News(models.Model):
     update_at = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
     is_published = models.BooleanField(default=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self): # ссылка на данный класс
+        return reverse('post', kwargs={'post_id': self.pk})
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self): # ссылка на данный класс
+        return reverse('category', kwargs={'cat_id': self.pk})
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,11 +1,57 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import *
+
+menu = [{'title': 'О сайте', 'url_name': 'about'},
+        {'title': 'Добавить статью', 'url_name': 'add_page'},
+        {'title': 'Обратная связь', 'url_name': 'contact'},
+        {'title': 'Войти', 'url_name': 'login'},
+        ]
 
 
 def index(request):
-    # print(request)
-    return HttpResponse('Hello world!')
+    posts = News.objects.all()
+    cats = Category.objects.all()
+    context = {'posts': posts,
+               'menu': menu,
+               'cats': cats,
+               'title': 'big web',
+               'cat_selected': 0,
+               }
+    return render(request, 'news/index.html', context=context)
 
 
-def test(request, testid):
-    return HttpResponse(f'this text %{testid}')
+def about(request):
+    return render(request, 'news/about.html', {'menu': menu, 'title': 'low web'})
+
+
+def addpage(request):
+    return HttpResponse('1')
+
+
+def contact(request):
+    return HttpResponse('2')
+
+
+def login(request):
+    return HttpResponse('3')
+
+
+def show_post(request, post_id):
+    return HttpResponse(f'sdadasd{post_id}')
+
+
+def show_category(request, cat_id):
+    posts = News.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    # if len(posts) == 0:
+    #     raise Http404()
+
+    context = {'posts': posts,
+               'menu': menu,
+               'cats': cats,
+               'title': 'Отображение про категориям',
+               'cat_selected': cat_id,
+               }
+    return render(request, 'news/index.html', context=context)
