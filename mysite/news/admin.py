8e3c23984da0b1bbf931fde47e59efe203d0ae42
Daniil <dankulagin@mail.ru>
2023-02-16 17:20:07 +0300

    add admin interfase

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'photo', 'is_published')  # то что видино на дисплее
+    list_display_links = ('id', 'title')  # поля на которые можно кликнуть и перейти на соответствующию статью
+    search_fields = ('title', 'content')  # поля по которым можно отсортировать
+    list_editable = ('is_published',)  # список редактируемых полей
+    list_filter = ("is_published", 'created_at')  # список полей по которым можэно будет фильтровать
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
--- mysite/news/models.py
+++ mysite/news/models.py
@@ -3,26 +3,36 @@ from django.urls import reverse
 
 
 class News(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    title = models.CharField(max_length=100, verbose_name="Заголовок")
+    content = models.TextField(blank=True, verbose_name="Контент")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время публикации")
+    update_at = models.DateTimeField(auto_now=True, verbose_name="Время обновления")
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name="Фото")
+    is_published = models.BooleanField(default=True, verbose_name="Публикация")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name="Категории")
 
     def __str__(self):
         return self.title
 
-    def get_absolute_url(self): # ссылка на данный класс
+    def get_absolute_url(self):  # ссылка на данный класс
         return reverse('post', kwargs={'post_id': self.pk})
+
+    class Meta:
+        verbose_name = 'новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at', 'title']
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Категории')
 
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self): # ссылка на данный класс
+    def get_absolute_url(self):  # ссылка на данный класс
         return reverse('category', kwargs={'cat_id': self.pk})
+
+    class Meta:
+        verbose_name = 'категорию'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
